22296383af6560afa68cda8ed1b543e1d591e836
Maik Riechert <maik.riechert@arcor.de>
2016-04-06 00:21:36 +0100

    improve domain -> coverage transform (checkerboard for grids)

--- src/transform.js
+++ src/transform.js
@@ -11,24 +11,59 @@ import {subsetCoverageByIndex, subsetCoverageByValue} from './subset.js'
 export function toCoverage (domain) {
   checkDomain(domain)
   
-  let dummyKey = 'foo'
-  let dummyLabel = 'bar'
+  let dummyKey = 'domain'
+  let dummyLabel = 'Domain'
+  
+  let assumeGrid = domain.axes.has('x') && domain.axes.has('y') && 
+                   (domain.axes.get('x').values.length > 1 || domain.axes.get('y').values.length > 1)
+  let categories
+  let categoryEncoding
+  const a = 'a'
+  const av = 0
+  const b = 'b'
+  const bv = 1
+  if (assumeGrid) {
+    categories = [{
+      id: a,
+      label: {en: 'A'}
+    }, {
+      id: b,
+      label: {en: 'B'}
+    }]
+    categoryEncoding = new Map([[a, [av]], [b, [bv]]])
+  } else {
+    categories = [{
+      id: a,
+      label: {en: 'X'}
+    }]
+    categoryEncoding = new Map([[a, [av]]])
+  }
     
   let parameters = new Map()
   parameters.set(dummyKey, {
     key: dummyKey,
     observedProperty: {
-      label: {en: dummyLabel}
-    }
+      label: {en: dummyLabel},
+      categories
+    },
+    categoryEncoding
   })
   
   let shape = new Map([...domain.axes].map(([name, axis]) => [name, axis.values.length]))
   
-  let loadRange = key => Promise.resolve({
+  let get
+  if (assumeGrid) {
+    // checkerboard pattern to see grid cells
+    let isOdd = n => n % 2
+    get = ({x=0, y=0}) => isOdd(x+y) ? av : bv
+  } else {
+    get = () => av
+  }
+  
+  let loadRange = () => Promise.resolve({
     shape,
-    dataType: 'float',
-    // TODO have persistent range values + some pattern for x/y grids (checkerboard)
-    get: obj => Math.random()
+    dataType: 'integer',
+    get
   })
 
   let cov = {

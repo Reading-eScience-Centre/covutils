2b55f7d2ac17e6c8cb46c4a2d70dacb0d53a41a4
Maik Riechert <maik.riechert@arcor.de>
2016-05-16 12:01:31 +0100

    add new modules to classic bundle

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import * as array from './array.js'
 import * as constants from './constants.js'
+import * as i18n from './i18n.js'
+import * as range from './range.js'
 import * as referencing from './referencing.js'
 import * as subset from './subset.js'
 import * as transform from './transform.js'
